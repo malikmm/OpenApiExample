5117367ec3268baec80cded4a26bc5c4a34b2e33
malikmm <malikmm@hotmail.com>
2024-02-06 11:16:42 +0000

    Project setup

---
+++ apifirstexample/src/test/java/com/malik/apifirstexample/ApifirstexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malik.apifirstexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
